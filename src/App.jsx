f032c4659df2d5297770368456ae969ce04b21de
AliyuAliyu1 <109520063+AliyuAliyu1@users.noreply.github.com>
2025-10-13 11:14:08 +0100

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import HomePage from '@/pages/HomePage';
+import ProductsPage from '@/pages/ProductsPage';
+import AboutPage from '@/pages/AboutPage';
+import ContactPage from '@/pages/ContactPage';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/products" element={<ProductsPage />} />
+      <Route path="/about" element={<AboutPage />} />
+      <Route path="/contact" element={<ContactPage />} />
+    </Routes>
+  );
+}
+
+export default App;
