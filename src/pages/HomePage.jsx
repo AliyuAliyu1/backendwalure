78a91176c3189e9f303fb6f5ef85c19c0de3d67d
AliyuAliyu1 <109520063+AliyuAliyu1@users.noreply.github.com>
2025-10-16 14:42:25 +0100

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -217,7 +217,7 @@ const HomePage = () => {
                 className="rounded-2xl overflow-hidden shadow-lg"
               >
                 <img
-                  src="/images/WhatsApp Image 2025-10-05 at 20.02.32_dcbc92a9.jpg"
+                  src="/images/pineapple.jpg"
                   alt="Lush field"
                   className="w-full h-full object-cover"
                 />
@@ -250,7 +250,7 @@ const HomePage = () => {
                 className="rounded-2xl overflow-hidden shadow-lg"
               >
                 <img
-                  src="/images/maiz.jpg"
+                  src="/images/soyabean.jpg"
                   alt="Production"
                   className="w-full h-full object-cover"
                 />
