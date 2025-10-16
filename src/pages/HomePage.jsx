5888ac05b103cd51455a8e53d5827ed053a7fc98
AliyuAliyu1 <109520063+AliyuAliyu1@users.noreply.github.com>
2025-10-16 14:37:19 +0100

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -265,7 +265,7 @@ const HomePage = () => {
                 className="rounded-2xl overflow-hidden shadow-lg"
               >
                 <img
-                  src="/images/WhatsApp Image 2025-10-05 at 20.05.45_86247bc5.jpeg"
+                  src="/images/WhatsApp Image 2025-10-05 at 20.05.45_86247bc5.jpg"
                   alt="Partner farm"
                   className="w-full h-full object-cover"
                 />
